9f055f7b33f6069313e219fb8dce4642e421304f
mohammadrezanajari <mr.najari@gmail.com>
2020-03-24 01:00:19 +0430

    first commit

---
+++ src/test/java/rez/school/SchoolApplicationTests.java
@@ -0,0 +1,13 @@
+package rez.school;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
