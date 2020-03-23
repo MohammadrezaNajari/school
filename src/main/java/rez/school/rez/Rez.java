0927d17ac21fe4ba2d8fb6eef036da85a8b73065
mohammadrezanajari <mr.najari@gmail.com>
2020-03-24 01:03:42 +0430

    new

--- src/main/java/rez/school/rez/Rez.java
+++ src/main/java/rez/school/rez/Rez.java
@@ -1,4 +1,14 @@
 package rez.school.rez;
 
 public class Rez {
+
+    private String rez;
+
+    public String getRez() {
+        return rez;
+    }
+
+    public void setRez(String rez) {
+        this.rez = rez;
+    }
 }
